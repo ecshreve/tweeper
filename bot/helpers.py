65c3b26fd2ed6e0db69503a40ba3102d2080332f
Eric Shreve <ecshreve13@gmail.com>
2021-08-27 04:32:54 -0700

    bot: clean up helper funcs

--- bot/helpers.py
+++ bot/helpers.py
@@ -1,6 +1,6 @@
 import json
-
-import pprint
+import os.path
+import pickle
 
 
 def CleanQuote(quote_object):
@@ -9,8 +9,9 @@ def CleanQuote(quote_object):
 
 
 def ParseJson():
-    ret = {}
     try:
+        ret = {}
+
         # Load the JSON file into a dictionary.
         with open("../data/quotes.json") as infile:
             data = json.load(infile)
@@ -24,7 +25,27 @@ def ParseJson():
                 ret[show_name] = []
             ret[show_name].append(cq)
 
+        # Pickle our nice clean dict so we can use it later without having to parse
+        # the JSON file again.
+        with open('data.pickle', 'wb') as f:
+            pickle.dump(ret, f, pickle.HIGHEST_PROTOCOL)
+
     except Exception as e:
         print(e)
 
-    return ret
+
+def SetupData():
+    try:
+        # Check if a pickle file already exists, if not then parse the JSON file
+        # and pickle it.
+        if not os.path.isfile('data.pickle'):
+            ParseJson()
+
+        with open('data.pickle', 'rb') as f:
+            # The protocol version used is detected automatically, so we do not
+            # have to specify it.
+            data = pickle.load(f)
+            return data
+
+    except Exception as e:
+        print(e)
